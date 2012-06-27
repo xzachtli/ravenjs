53e9644ce2a5863de97510959002c3bbe370945a
rrao <rrao@microsoft.com>
2012-06-27 16:42:38 -0700

    Fixed bug in hiloIdGenerator using POST instead of PUT and checking for wrong response codes.

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ var raven = require('./raven');
 raven.server('http://localhost:8080');
 var client = raven.connect();
 
-client.get('bar23', function(error, doc) {
-	console.log(doc);
-	doc.Price = 200;
+var doc = {
+	name: 'Ritesh',
+	last: 'Rao'
+};
+
+client.save(doc, function(error) {
+	console.log(error);
+	doc = {
+		name: 'Ritesh',
+		last: 'Rao'
+	};
 	client.save(doc, function(error) {
 		console.log(error);
 	});
