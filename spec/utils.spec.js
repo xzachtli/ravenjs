ddd06981d7b3cba373fe3610e3966026ba5e0b19
Ritesh Rao <rao.ritesh@gmail.com>
2012-07-03 16:55:41 -0700

    More refactoring

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -17,7 +17,7 @@ describe('utils', function() {
 
 		it('should return false for function', function() {
 			expect(utils.isString(function() { })).not.toBeTruthy();
-		})
+		});
 
 		it('should return false for number', function() {
 			expect(utils.isString(1)).not.toBeTruthy();
@@ -26,7 +26,7 @@ describe('utils', function() {
 		it('should return true for strings', function() {
 			expect(utils.isString('Foo')).toBeTruthy();
 		});
-	}); 
+	});
 
 	describe('.isNumber()', function() {
 		it('should return false for undefined', function() {
@@ -51,7 +51,7 @@ describe('utils', function() {
 
 		it('should return true for number', function() {
 			expect(utils.isNumber(1)).toBeTruthy();
-		})
+		});
 	});
 
 	describe('.isFunction()', function() {
@@ -99,10 +99,39 @@ describe('utils', function() {
 
 		it('should return false for number', function() {
 			expect(utils.isObject(1)).not.toBeTruthy();
-		})
+		});
 
 		it('should return true for object', function() {
 			expect(utils.isObject({ })).toBeTruthy();
 		});
-	})
+	});
+
+	describe('.clone()', function() {
+		it('should return original value if source is not an object', function() {
+			var cloned = utils.clone(1);
+			expect(cloned).toBe(1);
+		});
+
+		it('should create a cloned array when source is an array', function() {
+			var source = ['foo', 'bar'];
+			var cloned = utils.clone(source);
+			expect(cloned).toBeDefined();
+			expect(cloned.length).toBe(2);
+
+			cloned.push('baz');
+			expect(source.length).toBe(2);
+			expect(cloned.length).toBe(3);
+		});
+
+		it('should create a cloned object when source is an object', function() {
+			var source = { firstName: 'foo' };
+			var cloned = utils.clone(source);
+			expect(cloned).toBeDefined();
+			expect(cloned.firstName).toBeDefined();
+			expect(cloned.firstName).toBe(source.firstName);
+
+			cloned.firstName = 'Baz';
+			expect(source.firstName).toBe('foo');
+		});
+	});
 });
